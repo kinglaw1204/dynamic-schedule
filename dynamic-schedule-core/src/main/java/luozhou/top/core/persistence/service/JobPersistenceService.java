fd113a2538d6b37245fb2c5b646b63abb0be593e
xiaozhou <kinglaw1204@gmai.com>
2019-10-16 16:24:48 +0800

    change pom

---
+++ dynamic-schedule-core/src/main/java/luozhou/top/core/persistence/service/JobPersistenceService.java
@@ -0,0 +1,8 @@
+/**
+ * @description:
+ * @author: luozhou
+ * @create: 2019-10-16 15:58
+ **/
+public class JobPersistenceService {
+
+}
---
+++ dynamic-schedule-core/src/main/java/luozhou/top/core/persistence/service/impl/JobPersistenceServiceImpl.java
@@ -0,0 +1,8 @@
+/**
+ * @description:
+ * @author: luozhou
+ * @create: 2019-10-16 15:59
+ **/
+public class JobPersistenceServiceImpl {
+
+}
